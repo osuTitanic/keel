618cfc1f3520210a4ad40841e7e4c87ff1efc5e5
Lekuru <contact@lekuru.xyz>
2025-01-11 20:58:44 +0100

    Add `/account/friends` endpoint

--- app/routes/account/__init__.py
+++ app/routes/account/__init__.py
@@ -1,4 +1,6 @@
 
 from fastapi import APIRouter
+from . import friends
 
 router = APIRouter()
+router.include_router(friends.router, tags=['friends'])
--- app/routes/account/friends.py
+++ app/routes/account/friends.py
@@ -1,4 +1,19 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends
+from starlette.authentication import requires
+from app.common.database import relationships
+from app.models import UserModelCompact
+from sqlalchemy.orm import Session
+from typing import List
+import app
 
 router = APIRouter()
+
+@router.get('/friends', response_model=List[UserModelCompact])
+@requires('authenticated')
+def friends(request: Request, session: Session = Depends(app.database)):
+    friends = relationships.fetch_many_by_id(request.user.id, session)
+    return [
+        UserModelCompact.model_validate(friend.target, from_attributes=True)
+        for friend in friends
+    ]

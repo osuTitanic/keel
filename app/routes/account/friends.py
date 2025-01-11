71629552273b4f3cd1e37d7d77d540a06a477b04
Lekuru <contact@lekuru.xyz>
2025-01-11 21:59:09 +0100

    Refactor `/friends` endpoint to use session

--- app/routes/account/friends.py
+++ app/routes/account/friends.py
@@ -11,9 +11,8 @@ router = APIRouter()
 
 @router.get('/friends', response_model=List[UserModelCompact])
 @requires('authenticated')
-def friends(request: Request, session: Session = Depends(app.database)):
-    friends = relationships.fetch_many_by_id(request.user.id, session)
+def friends(request: Request):
     return [
         UserModelCompact.model_validate(friend.target, from_attributes=True)
-        for friend in friends
+        for friend in relationships.fetch_many_by_id(request.user.id, request.state.db)
     ]

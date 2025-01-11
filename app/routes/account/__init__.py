a5dcd687eb2ba4b27042df3f18c6de944bd04f56
Lekuru <contact@lekuru.xyz>
2025-01-11 21:59:21 +0100

    Add `/account/profile` endpoint

--- app/routes/account/__init__.py
+++ app/routes/account/__init__.py
@@ -1,6 +1,9 @@
 
 from fastapi import APIRouter
+
+from . import profile
 from . import friends
 
 router = APIRouter()
+router.include_router(profile.router, tags=['profile'])
 router.include_router(friends.router, tags=['friends'])
--- app/routes/account/profile.py
+++ app/routes/account/profile.py
@@ -1,4 +1,14 @@
 
-from fastapi import APIRouter
+from starlette.authentication import requires
+from fastapi import APIRouter, Request
+from app.models import UserModel
 
 router = APIRouter()
+
+@router.get('/profile')
+@requires('authenticated')
+def user_profile(request: Request) -> UserModel:
+    return UserModel.model_validate(
+        request.user,
+        from_attributes=True
+    )

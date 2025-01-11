06756df8b0b22d64f1c2a32e52cf5c747fba1c22
Lekuru <contact@lekuru.xyz>
2025-01-11 20:52:48 +0100

    Add session attributes to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 
-from . import exceptions
+
 from . import session
-from . import common
-from . import models
+
+database = session.database.yield_session
+requests = session.requests
+storage = session.storage
+logger = session.logger
+redis = session.redis
 
 from .server import api
 from . import middleware
+from . import exceptions
+from . import common
+from . import models

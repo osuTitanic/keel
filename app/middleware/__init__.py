8b7d7397f9e2aba07bc5c7f716fea5ef6f7732ba
Lekuru <contact@lekuru.xyz>
2025-01-12 21:54:08 +0100

    Add ratelimiting middleware

--- app/middleware/__init__.py
+++ app/middleware/__init__.py
@@ -1,3 +1,4 @@
 
 from . import authentication
+from . import ratelimiting
 from . import state
---
+++ app/middleware/ratelimiting.py
@@ -0,0 +1,45 @@
+
+from __future__ import annotations
+from app.common.helpers import ip
+from app.models import ErrorResponse
+from fastapi.responses import JSONResponse
+from fastapi import Request
+from typing import Callable
+from redis import Redis
+
+import asyncio
+import config
+import app
+
+async def run_async(func: Callable, *args):
+    return await asyncio.get_event_loop().run_in_executor(
+        None, func, *args
+    )
+
+def error_response(status_code: int, detail: str):
+    return JSONResponse(
+        status_code=status_code,
+        content=ErrorResponse(error=status_code, details=detail).model_dump()
+    )
+
+async def ratelimit_middleware(request: Request, call_next: Callable):
+    ip_address = ip.resolve_ip_address_fastapi(request)
+    redis: Redis = request.state.redis
+
+    # Get current request count
+    current = await run_async(redis.get, f'ratelimit:{ip_address}') or b'0'
+
+    # Check for rate limit
+    if int(current) > config.API_RATELIMIT_LIMIT:
+        return error_response(429, 'Rate limit exceeded')
+
+    # Set the new request count with expiry
+    await run_async(
+        redis.setex, f'ratelimit:{ip_address}',
+        config.API_RATELIMIT_WINDOW, int(current) + 1
+    )
+
+    return await call_next(request)
+
+if config.API_RATELIMIT_ENABLED:
+    app.api.middleware("http")(ratelimit_middleware)

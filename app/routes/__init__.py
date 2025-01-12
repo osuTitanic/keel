0f16d086143e330fe05d0c091b65c43dd117c6f9
Lekuru <contact@lekuru.xyz>
2025-01-12 21:12:54 +0100

    Use redis from request state

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,5 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from app.models import ServerStatsModel
 
 from . import account
@@ -13,10 +13,10 @@ router.include_router(oauth.router, prefix="/oauth", tags=["oauth"])
 router.include_router(account.router, prefix="/account", tags=["account"])
 
 @router.get('/stats', response_model=ServerStatsModel)
-def server_stats():
+def server_stats(request: Request):
     return ServerStatsModel(
         uptime=round(time.time() - app.session.startup_time),
-        total_scores=int(app.session.redis.get("bancho:totalscores") or 0),
-        total_users=int(app.session.redis.get("bancho:totalusers") or 0),
-        online_users=int(app.session.redis.get("bancho:users") or 0)
+        total_scores=int(request.state.redis.get("bancho:totalscores") or 0),
+        total_users=int(request.state.redis.get("bancho:totalusers") or 0),
+        online_users=int(request.state.redis.get("bancho:users") or 0)
     )

bdd96c9b8b44f9c8703f96c6d53c1baaa50589f1
Lekuru <contact@lekuru.xyz>
2025-01-13 20:41:57 +0100

    a

--- app/exceptions.py
+++ app/exceptions.py
@@ -7,25 +7,6 @@ from fastapi import Request, HTTPException
 from .models import ErrorResponse, ValidationErrorResponse, ValidationErrorModel
 from .server import api
 
-@api.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    response = ValidationErrorResponse(
-        errors=[
-            ValidationErrorModel(
-                loc=error['loc'],
-                msg=error['msg'],
-                type=error['type'],
-                input=str(error.get('input', '')) or None
-            )
-            for error in exc.errors()
-        ]
-    )
-
-    return JSONResponse(
-        response.model_dump(),
-        status_code=400
-    )
-
 @api.exception_handler(HTTPException)
 async def http_exception_handler(request: Request, exc: HTTPException):
     return JSONResponse(
@@ -44,4 +25,23 @@ async def starlette_exception_handler(request: Request, exc: StarletteHTTPExcept
             details=exc.detail
         ).model_dump(),
         status_code=exc.status_code
+    )
+
+@api.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    response = ValidationErrorResponse(
+        errors=[
+            ValidationErrorModel(
+                loc=error['loc'],
+                msg=error['msg'],
+                type=error['type'],
+                input=str(error.get('input', '')) or None
+            )
+            for error in exc.errors()
+        ]
+    )
+
+    return JSONResponse(
+        response.model_dump(),
+        status_code=400
     )

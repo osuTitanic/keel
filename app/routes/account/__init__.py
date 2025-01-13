c2ed74637481715f24f5a4657be956c34ceb44ab
Lekuru <contact@lekuru.xyz>
2025-01-13 21:31:18 +0100

    Include stubbed security endpoints

--- app/routes/account/__init__.py
+++ app/routes/account/__init__.py
@@ -1,12 +1,14 @@
 
 from fastapi import APIRouter
 
+from . import security
 from . import register
 from . import profile
 from . import friends
 from . import reset
 
 router = APIRouter()
+router.include_router(security.router)
 router.include_router(register.router)
 router.include_router(profile.router)
 router.include_router(friends.router)
--- app/routes/account/security.py
+++ app/routes/account/security.py
@@ -1,4 +1,12 @@
 
-from fastapi import APIRouter
+from fastapi import HTTPException, APIRouter, Request
 
 router = APIRouter()
+
+@router.post("/security/email")
+def change_email(request: Request):
+    raise HTTPException(501, "Not implemented") # TODO
+
+@router.post("/security/password")
+def change_password(request: Request):
+    raise HTTPException(501, "Not implemented") # TODO

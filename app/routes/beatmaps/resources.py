2398d36348a0f85b833a8dcfb2357784ed78e933
Lekuru <contact@lekuru.xyz>
2025-01-14 18:48:39 +0100

    Add `/beatmaps/<id>/file` endpoint

--- app/routes/beatmaps/resources.py
+++ app/routes/beatmaps/resources.py
@@ -1,4 +1,20 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Request
+from fastapi.responses import PlainTextResponse
 
 router = APIRouter()
+
+@router.get('/{id}/file')
+def internal_beatmap_file(request: Request, id: int):
+    osu = request.state.storage.get_beatmap_internal(id)
+
+    if not osu:
+        raise HTTPException(
+            status_code=404,
+            detail="The requested beatmap could not be found"
+        )
+
+    return PlainTextResponse(
+        content=osu,
+        headers={'Content-Disposition': f'attachment; filename={id}.osu'}
+    )

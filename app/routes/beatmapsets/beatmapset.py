2bbe598dee2e5a2746563c41bb332c62c95d7ff3
Lekuru <contact@lekuru.xyz>
2025-01-15 20:15:52 +0100

    Add `/beatmapsets/<id>` endpoint

--- app/routes/beatmapsets/beatmapset.py
+++ app/routes/beatmapsets/beatmapset.py
@@ -1,4 +1,26 @@
 
-from fastapi import APIRouter
+from fastapi import HTTPException, APIRouter, Request
+from app.models import BeatmapsetModel, ErrorResponse
+from app.common.database import beatmapsets
 
-router = APIRouter()
+router = APIRouter(
+    responses={
+        404: {"model": ErrorResponse, "description": "The requested beatmap could not be found"}
+    }
+)
+
+@router.get("/{id}", response_model=BeatmapsetModel)
+def get_beatmapset(request: Request, id: int) -> BeatmapsetModel:
+    if not (beatmapset := beatmapsets.fetch_one(id, request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested beatmap could not be found"
+        )
+
+    if beatmapset.status <= -3:
+        raise HTTPException(
+            status_code=404,
+            detail="The requested beatmap could not be found"
+        )
+
+    return BeatmapsetModel.model_validate(beatmapset, from_attributes=True)

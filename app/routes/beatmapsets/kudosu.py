45be3937ddeadc0ee043dc36cbe97d685701c929
Lekuru <contact@lekuru.xyz>
2025-01-16 22:01:05 +0100

    Add `/beatmapsets/<id>/kudosu` endpoint

--- app/routes/beatmapsets/kudosu.py
+++ app/routes/beatmapsets/kudosu.py
@@ -1,4 +1,29 @@
 
-from fastapi import APIRouter
+from app.models import KudosuModel, ErrorResponse
+from app.common.database import beatmapsets, modding
+from fastapi import APIRouter, HTTPException, Request
+from typing import List
 
-router = APIRouter()
+router = APIRouter(
+    responses={
+        404: {"model": ErrorResponse, "description": "The requested beatmap could not be found"}
+    }
+)
+
+@router.get("/{set_id}/kudosu", response_model=List[KudosuModel])
+def get_kudosu_by_set(request: Request, set_id: int):
+    if not (beatmapset := beatmapsets.fetch_one(set_id, request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested beatmapset could not be found"
+        )
+
+    kudosu = modding.fetch_all_by_set(
+        set_id=beatmapset.id,
+        session=request.state.db
+    )
+
+    return [
+        KudosuModel.model_validate(k, from_attributes=True)
+        for k in kudosu
+    ]

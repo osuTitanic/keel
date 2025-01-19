8ddcb593f5ddee10f31c998b4e5655104745cff8
Lekuru <contact@lekuru.xyz>
2025-01-19 14:15:57 +0100

    Add `/<set_id>/nominations` endpoint

--- app/routes/beatmapsets/nomination.py
+++ app/routes/beatmapsets/nomination.py
@@ -1,4 +1,16 @@
 
-from fastapi import APIRouter
+from __future__ import annotations
+from fastapi import APIRouter, Request
+from typing import List
+
+from app.common.database import nominations
+from app.models import NominationModel
 
 router = APIRouter()
+
+@router.get('/{set_id}/nominations', response_model=List[NominationModel])
+def get_nominations(request: Request, set_id: int):
+    return [
+        NominationModel.model_validate(nom, from_attributes=True)
+        for nom in nominations.fetch_by_beatmapset(set_id, request.state.db)
+    ]

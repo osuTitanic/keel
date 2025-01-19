0c68ef79708cc2bf8f124a4aaaa4fb53327024cd
Lekuru <contact@lekuru.xyz>
2025-01-19 15:50:06 +0100

    Implement beatmapset resource endpoints

--- app/routes/beatmapsets/resources.py
+++ app/routes/beatmapsets/resources.py
@@ -1,25 +1,56 @@
 
-from fastapi import APIRouter
-from zipfile import ZipFile
-from io import BytesIO
+from fastapi import HTTPException, APIRouter, Request, Query
+from fastapi.responses import StreamingResponse, Response
+from app.common.database import beatmapsets
 
 router = APIRouter()
 
-def remove_video_from_zip(osz: bytes) -> bytes:
-    video_extensions = [
-        ".wmv", ".flv", ".mp4", ".avi", ".m4v"
-    ]
+@router.get('/{id}/osz')
+def get_osz(request: Request, id: int, no_video: bool = Query(False)) -> StreamingResponse:
+    if not (beatmapset := beatmapsets.fetch_one(id, request.state.db)):
+        raise HTTPException(status_code=404)
 
-    with ZipFile(BytesIO(osz), 'r') as zip_file:
-        files_to_keep = [
-            item for item in zip_file.namelist()
-            if not any(item.lower().endswith(ext) for ext in video_extensions)
-        ]
+    if not beatmapset.available:
+        raise HTTPException(status_code=451)
 
-        output = BytesIO()
+    if not (response := request.state.storage.api.osz(id, no_video)):
+        raise HTTPException(404)
 
-        with ZipFile(output, 'w') as output_zip:
-            for file in files_to_keep:
-                output_zip.writestr(file, zip_file.read(file))
+    return StreamingResponse(
+        response.iter_content(1024*256),
+        media_type='application/octet-stream',
+        headers={
+            'Content-Disposition': f'attachment; filename={id} {beatmapset.artist} - {beatmapset.title}.osz',
+            'Content-Length': response.headers.get('Content-Length', 0)
+        }
+    )
 
-        return output.getvalue()
+@router.get('/{id}/background', response_class=Response)
+def get_background_large(request: Request, id: int) -> Response:
+    if not (file := request.state.storage.get_background(f'{id}l')):
+        raise HTTPException(404)
+
+    return Response(
+        file,
+        media_type='image/jpeg'
+    )
+
+@router.get('/{id}/background/small', response_class=Response)
+def get_background_small(request: Request, id: int) -> Response:
+    if not (file := request.state.storage.get_background(f'{id}')):
+        raise HTTPException(404)
+
+    return Response(
+        file,
+        media_type='image/jpeg'
+    )
+
+@router.get('/{id}/audio', response_class=Response)
+def get_audio_preview(request: Request, id: int) -> Response:
+    if not (file := request.state.storage.get_mp3(id)):
+        raise HTTPException(404)
+
+    return Response(
+        file,
+        media_type='audio/mpeg'
+    )

fb70e7088f5d8e443e070e02719bc66cd2a36ae7
Lekuru <contact@lekuru.xyz>
2025-01-24 19:00:45 +0100

    Remove `submit` router

--- app/routes/chat/__init__.py
+++ app/routes/chat/__init__.py
@@ -2,11 +2,10 @@
 from fastapi import APIRouter
 
 from . import channels
-from . import submit
 from . import search
 from . import dms
 
 router = APIRouter()
 router.include_router(channels.router)
-router.include_router(submit.router)
 router.include_router(search.router)
+router.include_router(dms.router)

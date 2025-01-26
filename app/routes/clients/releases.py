44b178b81013ef8c8c5a1f590a693a9e28f22422
Lekuru <contact@lekuru.xyz>
2025-01-26 20:07:14 +0100

    Add clients endpoint

--- app/routes/clients/releases.py
+++ app/routes/clients/releases.py
@@ -1,4 +1,15 @@
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from typing import List
+
+from app.common.database import releases
+from app.models import ClientModel
 
 router = APIRouter()
+
+@router.get("/", response_model=List[ClientModel])
+def get_client_releases(request: Request) -> List[ClientModel]:
+    return [
+        ClientModel.model_validate(client, from_attributes=True)
+        for client in releases.fetch_all(request.state.db)
+    ]

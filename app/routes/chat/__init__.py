20f13ef4d7f88dadbd3bb00bd50a1e3d6b470b46
Lekuru <contact@lekuru.xyz>
2025-01-26 19:54:59 +0100

    Add message submission api endpoints

--- app/routes/chat/__init__.py
+++ app/routes/chat/__init__.py
@@ -2,10 +2,12 @@
 from fastapi import APIRouter
 
 from . import channels
+from . import submit
 from . import search
 from . import dms
 
 router = APIRouter()
 router.include_router(channels.router)
+router.include_router(submit.router)
 router.include_router(search.router)
 router.include_router(dms.router)
--- app/routes/chat/submit.py
+++ app/routes/chat/submit.py
@@ -1,4 +1,128 @@
 
-from fastapi import APIRouter
 
-router = APIRouter()
+from fastapi import HTTPException, APIRouter, Request, Body
+from datetime import datetime, timedelta
+from sqlalchemy.orm import Session
+
+from app.models import MessagePostRequest, PrivateMessageModel, MessageModel
+from app.common.database import channels, groups, messages, users
+from app.common.constants.strings import BAD_WORDS
+from app.common.database.objects import DBUser
+from app.security import require_login
+from app.utils import requires
+
+router = APIRouter(dependencies=[require_login])
+
+@router.post('/channels/{target}/messages', response_model=MessageModel)
+@requires(['authenticated', 'activated', 'unsilenced', 'unrestricted'])
+def post_message(
+    target: str,
+    request: Request,
+    data: MessagePostRequest = Body(...)
+) -> MessageModel:
+    if not (channel := channels.fetch_one(target, request.state.db)):
+        raise HTTPException(404, 'Channel not found')
+
+    user_permissions = groups.get_player_permissions(
+        request.user.id,
+        request.state.db
+    )
+
+    if user_permissions < channel.read_permissions:
+        raise HTTPException(403, 'Insufficient permissions')
+
+    if user_permissions < channel.write_permissions:
+        raise HTTPException(403, 'Insufficient permissions')
+
+    has_bad_words = any([
+        word in data.message.lower()
+        for word in BAD_WORDS
+    ])
+
+    if has_bad_words and not request.user.is_bot:
+        silence_user(request.user.id, 60*10, request.state.db)
+        raise HTTPException(400, 'Message contains offensive language')
+
+    # TODO: Check if channel is moderated
+    # TODO: Check for message spamming
+
+    message = messages.create(
+        request.user.name,
+        channel.name,
+        data.message,
+        request.state.db
+    )
+    message.sender = request.user
+
+    request.state.events.submit(
+        'external_message',
+        sender_id=request.user.id,
+        sender=request.user.name,
+        target=channel.name,
+        message=data.message
+    )
+
+    return MessageModel.model_validate(
+        message,
+        from_attributes=True
+    )
+
+@router.post('/dms/{target_id}/messages', response_model=PrivateMessageModel)
+@requires(['authenticated', 'activated', 'unsilenced', 'unrestricted'])
+def post_private_message(
+    target_id: int,
+    request: Request,
+    data: MessagePostRequest = Body(...)
+) -> PrivateMessageModel:
+    if not (target := users.fetch_by_id(target_id, session=request.state.db)):
+        raise HTTPException(404, 'Target not found')
+
+    if is_user_silenced(target):
+        raise HTTPException(400, 'Target is silenced')
+
+    if target.id == request.user.id:
+        raise HTTPException(400, 'Cannot send messages to yourself')
+
+    # TODO: Check for message spamming
+    # TODO: Check if target blocked user / has friend-only dms
+
+    message = messages.create_private(
+        request.user.id,
+        target.id,
+        data.message,
+        request.state.db
+    )
+
+    request.state.events.submit(
+        'external_dm',
+        sender_id=request.user.id,
+        target_id=target.id,
+        message=data.message
+    )
+
+    return PrivateMessageModel.model_validate(
+        message,
+        from_attributes=True
+    )
+
+def silence_user(user: DBUser, duration: int, request: Request):
+    silence_offset = (
+        user.silence_end or datetime.now()
+    )
+
+    users.update(
+        user.id,
+        {"silence_end": silence_offset + timedelta(seconds=duration)},
+        session=request.state.db
+    )
+
+    # Send 'silence' event to bancho
+    request.state.events.submit(
+        'silence',
+        user_id=user.id,
+        duration=duration,
+        reason='Auto-silenced for using offensive language in chat.'
+    )
+
+def is_user_silenced(user: DBUser) -> bool:
+    return user.silence_end and user.silence_end > datetime.now()

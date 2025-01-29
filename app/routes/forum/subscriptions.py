9b297ace89409e862a1e0882e403400a1c39d69a
Lekuru <contact@lekuru.xyz>
2025-01-29 20:00:15 +0100

    Add get subscriptions endpoint

--- app/routes/forum/subscriptions.py
+++ app/routes/forum/subscriptions.py
@@ -1,4 +1,27 @@
 
-from fastapi import APIRouter
+from fastapi import HTTPException, APIRouter, Request
+from app.models import SubscriptionModel
+from app.security import require_login
+from app.common.database import users
+from app.utils import requires
 
-router = APIRouter()
+router = APIRouter(dependencies=[require_login])
+
+@router.get("/subscriptions")
+@requires("authenticated")
+def get_subscriptions(request: Request):
+    subscriptions = users.fetch_subscriptions(
+        request.user.id,
+        request.state.db
+    )
+
+    subscriptions = [
+        subscription
+        for subscription in subscriptions
+        if not subscription.topic.hidden
+    ]
+
+    return [
+        SubscriptionModel.model_validate(subscription, from_attributes=True)
+        for subscription in subscriptions
+    ]

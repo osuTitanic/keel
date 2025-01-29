4ccbe7071da7646ae453c19c86908c7ad5ab5205
Lekuru <contact@lekuru.xyz>
2025-01-29 19:19:39 +0100

    Add bbcode rendering endpoint

--- app/routes/forum/bbcode.py
+++ app/routes/forum/bbcode.py
@@ -1,4 +1,14 @@
 
-from fastapi import APIRouter
+from fastapi import Response, APIRouter, Request, Body
+from app.models import BBCodeRenderRequest
+from app import bbcode
 
 router = APIRouter()
+
+@router.post('/bbcode')
+def render_bbcode(request: Request, data: BBCodeRenderRequest = Body(...)):
+    return Response(
+        bbcode.render_html(data.input),
+        status_code=200,
+        media_type='text/html'
+    )

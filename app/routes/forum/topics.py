8485c9226ac4a4f48e871526889daebb3cb783f3
Lekuru <contact@lekuru.xyz>
2025-01-29 19:26:23 +0100

    Add `/forum/<forum_id>/topics` endpoint

--- app/routes/forum/topics.py
+++ app/routes/forum/topics.py
@@ -1,4 +1,33 @@
 
-from fastapi import APIRouter
+from fastapi import HTTPException, APIRouter, Request, Query
+from typing import List
+
+from app.common.database import forums, topics
+from app.models import TopicModel
 
 router = APIRouter()
+
+@router.get("/{forum_id}/topics")
+def get_forum_topics(
+    request: Request,
+    forum_id: int,
+    offset: int = Query(0, ge=0),
+    limit: int = Query(25, le=50)
+) -> List[TopicModel]:
+    if not (forum := forums.fetch_by_id(forum_id, request.state.db)):
+        raise HTTPException(404, "Forum not found")
+    
+    if forum.hidden:
+        raise HTTPException(404, "Forum not found")
+
+    forum_topics = topics.fetch_recent_many(
+        forum.id,
+        limit=limit,
+        offset=offset,
+        session=request.state.db
+    )
+
+    return [
+        TopicModel.model_validate(topic, from_attributes=True)
+        for topic in forum_topics
+    ]

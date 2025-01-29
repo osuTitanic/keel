e1e77d4a07bec6b6313c31b09097a7addfbbbe7c
Lekuru <contact@lekuru.xyz>
2025-01-29 19:23:14 +0100

    forgot to add response classes

--- app/routes/forum/forum.py
+++ app/routes/forum/forum.py
@@ -5,14 +5,14 @@ from app.models import ForumModel
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/", response_model=list[ForumModel])
 def get_main_forums(request: Request):
     return [
         ForumModel.model_validate(forum, from_attributes=True)
         for forum in forums.fetch_main_forums(request.state.db)
     ]
 
-@router.get("/{forum_id}")
+@router.get("/{forum_id}", response_model=ForumModel)
 def get_forum(request: Request, forum_id: int):
     if not (forum := forums.fetch_by_id(forum_id, request.state.db)):
         raise HTTPException(404, "Forum not found")

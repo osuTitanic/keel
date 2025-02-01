c80e63c02f8fea5c96127b7cca4d3cdcbc18542e
Lekuru <contact@lekuru.xyz>
2025-02-01 15:08:23 +0100

    Add country rankings endpoint

--- app/routes/rankings/__init__.py
+++ app/routes/rankings/__init__.py
@@ -1,9 +1,9 @@
 
 from fastapi import APIRouter
 
-from . import regular
 from . import country
+from . import regular
 
 router = APIRouter()
-router.include_router(regular.router)
 router.include_router(country.router)
+router.include_router(regular.router)
--- app/routes/rankings/country.py
+++ app/routes/rankings/country.py
@@ -1,4 +1,37 @@
 
-from fastapi import APIRouter
+from app.common.constants import COUNTRIES
+from app.common.cache import leaderboards
+from app.models import (
+    CountryEntryModel,
+    CountryStatsModel,
+    ModeType
+)
+
+from fastapi import Request, APIRouter
+from typing import List
 
 router = APIRouter()
+
+@router.get('/country/{mode}')
+def get_country_rankings(request: Request, mode: ModeType) -> List[CountryEntryModel]:
+    return [
+        CountryEntryModel(
+            index=index + 1,
+            country_acronym=country['name'],
+            country_name=resolve_country_name(country['name']),
+            stats=resolve_country_stats(country)
+        )
+        for index, country in enumerate(leaderboards.top_countries(mode.integer))
+    ]
+
+def resolve_country_stats(country: dict) -> CountryStatsModel:
+    return CountryStatsModel(
+        average_performance=country['average_pp'],
+        total_performance=country['total_performance'],
+        total_rscore=country['total_rscore'],
+        total_tscore=country['total_tscore'],
+        total_users=country['total_users']
+    )
+
+def resolve_country_name(acronym: str) -> str:
+    return COUNTRIES.get(acronym.upper(), 'Unknown')

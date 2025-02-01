c5addc98266e59122ff04d8dd1269e3bd250b9a9
Lekuru <contact@lekuru.xyz>
2025-02-01 14:19:59 +0100

    Add multiplayer endpoints

--- app/routes/multiplayer/events.py
+++ app/routes/multiplayer/events.py
@@ -1,4 +1,26 @@
 
-from fastapi import APIRouter
+from fastapi import HTTPException, APIRouter, Request, Query
+from datetime import datetime
+from typing import List
+
+from app.common.database import matches, events
+from app.models import MatchEventModel
 
 router = APIRouter()
+
+@router.get('/{id}/events', response_model=List[MatchEventModel])
+def get_match_events(
+    request: Request,
+    id: int,
+    after: datetime = Query(None)
+) -> List[MatchEventModel]:
+    if not (match := matches.fetch_by_id(id, request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested match could not be found"
+        )
+
+    return [
+        MatchEventModel.model_validate(event, from_attributes=True)
+        for event in events.fetch_all_after_time(match.id, after or match.created_at, request.state.db)
+    ]
--- app/routes/multiplayer/match.py
+++ app/routes/multiplayer/match.py
@@ -1,4 +1,16 @@
 
-from fastapi import APIRouter
+from fastapi import HTTPException, APIRouter, Request
+from app.common.database import matches
+from app.models import MatchModel
 
 router = APIRouter()
+
+@router.get('/{id}', response_model=MatchModel)
+def get_match(request: Request, id: int) -> MatchModel:
+    if not (match := matches.fetch_by_id(id, request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested match could not be found"
+        )
+
+    return MatchModel.model_validate(match, from_attributes=True)

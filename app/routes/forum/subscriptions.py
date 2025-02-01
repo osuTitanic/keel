7528107dc992a2f76be8fad9fda2ed5213c5255a
Lekuru <contact@lekuru.xyz>
2025-02-01 13:57:48 +0100

    Add forum subscription endpoints

--- app/routes/forum/subscriptions.py
+++ app/routes/forum/subscriptions.py
@@ -1,8 +1,8 @@
 
-from fastapi import HTTPException, APIRouter, Request
-from app.models import SubscriptionModel
+from fastapi import HTTPException, APIRouter, Request, Body
+from app.models import SubscriptionModel, SubscriptionRequest
+from app.common.database import users, topics
 from app.security import require_login
-from app.common.database import users
 from app.utils import requires
 
 router = APIRouter(dependencies=[require_login])
@@ -25,3 +25,60 @@ def get_subscriptions(request: Request):
         SubscriptionModel.model_validate(subscription, from_attributes=True)
         for subscription in subscriptions
     ]
+
+@router.get("/subscriptions/{topic_id}")
+@requires("authenticated")
+def get_subscription(request: Request, topic_id: int):
+    if not (topic := topics.fetch_one(topic_id, request.state.db)):
+        raise HTTPException(404, "Topic not found")
+
+    if topic.hidden:
+        raise HTTPException(404, "Topic not found")
+
+    subscription = users.fetch_subscription(
+        topic_id,
+        request.user.id,
+        request.state.db
+    )
+
+    if not subscription:
+        raise HTTPException(404, "Subscription not found")
+
+    return SubscriptionModel.model_validate(subscription, from_attributes=True)
+
+@router.post("/subscriptions", response_model=SubscriptionModel)
+@requires("authenticated")
+def create_subscription(request: Request, data: SubscriptionRequest = Body(...)):
+    if not (topic := topics.fetch_one(data.topic_id, request.state.db)):
+        raise HTTPException(404, "Topic not found")
+
+    if topic.hidden:
+        raise HTTPException(404, "Topic not found")
+
+    subscription = topics.add_subscriber(
+        topic.id,
+        request.user.id,
+        request.state.db
+    )
+
+    return SubscriptionModel.model_validate(subscription, from_attributes=True)
+
+@router.delete("/subscriptions/{topic_id}")
+@requires("authenticated")
+def delete_subscription(request: Request, topic_id: int) -> dict:
+    if not (topic := topics.fetch_one(topic_id, request.state.db)):
+        raise HTTPException(404, "Topic not found")
+
+    if topic.hidden:
+        raise HTTPException(404, "Topic not found")
+
+    rows = topics.delete_subscriber(
+        topic.id,
+        request.user.id,
+        request.state.db
+    )
+
+    if not rows:
+        raise HTTPException(404, "Subscription not found")
+    
+    return {}

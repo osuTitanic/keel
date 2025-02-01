58a7688894f4479a1acf18cdbf44f85abe1cd405
Lekuru <contact@lekuru.xyz>
2025-02-01 23:58:23 +0100

    Add `ScoreRecordsModel`

--- app/models/score.py
+++ app/models/score.py
@@ -53,3 +53,13 @@ class ScoreModelWithoutBeatmap(BaseModel):
     grade: str
     pinned: bool
     user: UserModel
+
+class ScoreRecordsModel(BaseModel):
+    osu_vanilla: ScoreModel
+    osu_relax: ScoreModel
+    osu_autopilot: ScoreModel
+    taiko_vanilla: ScoreModel
+    taiko_relax: ScoreModel
+    ctb_vanilla: ScoreModel
+    ctb_relax: ScoreModel
+    mania: ScoreModel

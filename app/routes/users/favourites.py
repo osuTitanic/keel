2e21aba3176a848d02d48e04969cc8ff4e75a1ce
Lekuru <contact@lekuru.xyz>
2025-02-03 23:59:07 +0100

    Add favourite delete endpoint

--- app/routes/users/favourites.py
+++ app/routes/users/favourites.py
@@ -29,6 +29,26 @@ def get_favourites(request: Request, user_id: int) -> List[FavouriteModel]:
         for favourite in user_favourites
     ]
 
+@router.get("/{user_id}/favourites/{set_id}")
+def get_favourite(request: Request, user_id: int, set_id: int) -> FavouriteModel:
+    if not (user := users.fetch_by_id(user_id, session=request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested user could not be found"
+        )
+
+    if not (favourite := favourites.fetch_one(user.id, set_id, request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="You have not favourited this beatmapset"
+        )
+
+    return FavouriteModel(
+        user=UserModelCompact.model_validate(user, from_attributes=True),
+        beatmapset=BeatmapsetModel.model_validate(favourite.beatmapset, from_attributes=True),
+        created_at=favourite.created_at
+    )
+
 @router.post("/{user_id}/favourites", response_model=FavouriteModel)
 def add_favourite(
     request: Request,
@@ -74,5 +94,34 @@ def add_favourite(
     return FavouriteModel(
         user=UserModelCompact.model_validate(user, from_attributes=True),
         beatmapset=BeatmapsetModel.model_validate(beatmapset, from_attributes=True),
+        created_at=favourite.created_at
+    )
+
+@router.delete("/{user_id}/favourites/{set_id}", response_model=FavouriteModel)
+def remove_favourite(request: Request, user_id: int, set_id: int):
+    if not (user := users.fetch_by_id(user_id, session=request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested user could not be found"
+        )
+
+    if not (favourite := favourites.fetch_one(user.id, set_id, request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="You have not favourited this beatmapset"
+        )
+
+    set_model = BeatmapsetModel.model_validate(favourite.beatmapset, from_attributes=True)
+    user_model = UserModelCompact.model_validate(user, from_attributes=True)
+
+    favourites.delete(
+        user.id,
+        favourite.set_id,
+        request.state.db
+    )
+
+    return FavouriteModel(
+        user=user_model,
+        beatmapset=set_model,
         created_at=favourite.created_at
     )

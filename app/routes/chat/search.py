c6b9170489d9968b48bcf71cd26bc460d5536731
Lekuru <contact@lekuru.xyz>
2025-02-05 17:25:44 +0100

    Add un-implemented search endpoint

--- app/routes/chat/search.py
+++ app/routes/chat/search.py
@@ -1,4 +1,9 @@
 
-from fastapi import APIRouter
+from fastapi import HTTPException, APIRouter, Request
 
 router = APIRouter()
+
+@router.post("/search")
+def search_chat_messages(request: Request):
+    """Currently not implemented"""
+    raise HTTPException(501, "Not implemented")

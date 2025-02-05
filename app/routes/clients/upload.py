7951345a2c45a7cb33eb6ad37184ddd72590f84b
Lekuru <contact@lekuru.xyz>
2025-02-05 17:30:41 +0100

    Add un-implemented upload endpoint

--- app/routes/clients/upload.py
+++ app/routes/clients/upload.py
@@ -1,4 +1,11 @@
 
-from fastapi import APIRouter
+from fastapi import HTTPException, APIRouter, Request
+from app.utils import requires
 
 router = APIRouter()
+
+@router.post("/")
+@requires("tmg")
+def upload_client(request: Request):
+    """Currently not implemented"""
+    raise HTTPException(501, "Not implemented")

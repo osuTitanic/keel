cf62f5ee67123f5cd8244cb3c954eaf83d76ef3d
Lekuru <contact@lekuru.xyz>
2025-02-05 16:42:34 +0100

    Refactor user activity endpoints

--- app/routes/users/activity.py
+++ app/routes/users/activity.py
@@ -1,8 +1,6 @@
 
 from app.common.database import activities, users
 from app.models import ActivityModel, ModeAlias
-from app.common.constants import GameMode
-
 from fastapi import HTTPException, APIRouter, Request
 from fastapi.responses import RedirectResponse
 from typing import List
@@ -26,10 +24,39 @@ def get_user_activity_by_preferred_mode(
             detail='The requested user was not found'
         )
 
-    return RedirectResponse(
-        f"/users/{user_id}/activity/{GameMode(user.preferred_mode).alias}",
-        status_code=302
+    user_activity = activities.fetch_recent(
+        user.id,
+        user.preferred_mode,
+        session=request.state.db
     )
+
+    # TODO: I really need to refactor the way how I store this...
+    recent_activity = [
+        (
+            activity,
+            zip(
+                activity.activity_links.split('||'),
+                activity.activity_args.split('||')
+            )
+        )
+        for activity in user_activity
+    ]
+
+    return [
+        ActivityModel(
+            id=activity.id,
+            user_id=activity.user_id,
+            mode=activity.mode,
+            time=activity.time,
+            activity=activity.activity_text.format(
+                *(
+                    f"[{text}]({link})"
+                    for link, text in args
+                )
+            )
+        )
+        for activity, args in recent_activity
+    ]
 
 @router.get('/{user_id}/activity/{mode}', response_model=List[ActivityModel])
 def get_user_activity(
@@ -49,6 +76,12 @@ def get_user_activity(
             detail='The requested user was not found'
         )
 
+    user_activity = activities.fetch_recent(
+        user.id,
+        mode.integer,
+        session=request.state.db
+    )
+
     # TODO: I really need to refactor the way how I store this...
     recent_activity = [
         (
@@ -58,7 +91,7 @@ def get_user_activity(
                 activity.activity_args.split('||')
             )
         )
-        for activity in activities.fetch_recent(int(user_id), mode.integer, session=request.state.db)
+        for activity in user_activity
     ]
 
     return [

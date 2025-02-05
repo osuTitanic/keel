face7bae71df24d806561a96b8986a213249bd7f
Lekuru <contact@lekuru.xyz>
2025-02-05 16:36:58 +0100

    Add top plays endpoint

--- app/routes/users/top.py
+++ app/routes/users/top.py
@@ -1,4 +1,89 @@
 
-from fastapi import APIRouter
+from app.models import ScoreCollectionResponse, ScoreModelWithoutUser, ModeAlias
+from app.common.database.repositories import scores, users
+from fastapi import HTTPException, Request, APIRouter, Query
 
 router = APIRouter()
+
+@router.get("/{user_id}/top", response_model=ScoreCollectionResponse)
+def get_top_plays_preferred_mode(
+    request: Request,
+    user_id: int,
+    offset: int = Query(0, ge=0),
+    limit: int = Query(50, ge=1, le=50)
+) -> ScoreCollectionResponse:
+    if not (user := users.fetch_by_id(user_id, session=request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested user could not be found"
+        )
+    
+    if not user.activated:
+        raise HTTPException(
+            status_code=404,
+            detail="The requested user was not found"
+        )
+
+    top_scores = scores.fetch_top_scores(
+        user.id,
+        user.preferred_mode,
+        offset=offset,
+        limit=limit,
+        session=request.state.db
+    )
+
+    top_count = scores.fetch_top_scores_count(
+        user.id,
+        user.preferred_mode,
+        request.state.db
+    )
+
+    return ScoreCollectionResponse(
+        total=top_count,
+        scores=[
+            ScoreModelWithoutUser.model_validate(score, from_attributes=True)
+            for score in top_scores
+        ]
+    )
+
+@router.get("/{user_id}/top/{mode}", response_model=ScoreCollectionResponse)
+def get_top_plays(
+    request: Request,
+    mode: ModeAlias,
+    user_id: int,
+    offset: int = Query(0, ge=0),
+    limit: int = Query(50, ge=1, le=50)
+) -> ScoreCollectionResponse:
+    if not (user := users.fetch_by_id(user_id, session=request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested user could not be found"
+        )
+    
+    if not user.activated:
+        raise HTTPException(
+            status_code=404,
+            detail="The requested user was not found"
+        )
+
+    top_scores = scores.fetch_top_scores(
+        user.id,
+        mode.integer,
+        offset=offset,
+        limit=limit,
+        session=request.state.db
+    )
+
+    top_count = scores.fetch_top_scores_count(
+        user.id,
+        mode.integer,
+        request.state.db
+    )
+
+    return ScoreCollectionResponse(
+        total=top_count,
+        scores=[
+            ScoreModelWithoutUser.model_validate(score, from_attributes=True)
+            for score in top_scores
+        ]
+    )

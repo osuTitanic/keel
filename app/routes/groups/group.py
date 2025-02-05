277fb7b49436b8b6a287f5c5e119be85304ebf44
Lekuru <contact@lekuru.xyz>
2025-02-05 20:07:49 +0100

    Add response samples to groups

--- app/routes/groups/group.py
+++ app/routes/groups/group.py
@@ -1,11 +1,12 @@
 
-from app.models import UserModelCompact, GroupModel
+from app.models import UserModelCompact, GroupModel, ErrorResponse
 from app.common.database import groups
 
 from fastapi import HTTPException, APIRouter, Request
 from typing import List
 
 router = APIRouter()
+responses = {404: {"description": "The requested group could not be found", "model": ErrorResponse}}
 
 @router.get('/', response_model=List[GroupModel])
 def get_groups(request: Request) -> List[GroupModel]:
@@ -14,7 +15,7 @@ def get_groups(request: Request) -> List[GroupModel]:
         for group in groups.fetch_all(session=request.state.db)
     ]
 
-@router.get('/{id}', response_model=GroupModel)
+@router.get('/{id}', response_model=GroupModel, responses=responses)
 def get_group(request: Request, id: int) -> GroupModel:
     if not (group := groups.fetch_one(id, request.state.db)):
         raise HTTPException(
@@ -30,7 +31,7 @@ def get_group(request: Request, id: int) -> GroupModel:
 
     return GroupModel.model_validate(group, from_attributes=True)
 
-@router.get('/{id}/users', response_model=List[UserModelCompact])
+@router.get('/{id}/users', response_model=List[UserModelCompact], responses=responses)
 def get_group_users(request: Request, id: int) -> List[UserModelCompact]:
     if not (group := groups.fetch_one(id, request.state.db)):
         raise HTTPException(

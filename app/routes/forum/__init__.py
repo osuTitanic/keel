97d1a539a585ed8ea6e44ebb4cf64cec0e03db39
Lekuru <contact@lekuru.xyz>
2025-05-26 21:45:17 +0200

    Add endpoint to upload images

--- app/routes/forum/__init__.py
+++ app/routes/forum/__init__.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 from . import subscriptions
 from . import bookmarks
 from . import bbcode
+from . import images
 from . import topics
 from . import forum
 from . import posts
@@ -12,6 +13,7 @@ router = APIRouter()
 router.include_router(subscriptions.router)
 router.include_router(bookmarks.router)
 router.include_router(bbcode.router)
+router.include_router(images.router)
 router.include_router(topics.router)
 router.include_router(forum.router)
 router.include_router(posts.router)
---
+++ app/routes/forum/images.py
@@ -0,0 +1,32 @@
+
+from fastapi import APIRouter, Request, UploadFile, File
+from fastapi.responses import JSONResponse
+from app.utils import requires
+from requests import Response
+from time import time
+
+router = APIRouter()
+
+@router.post("/images")
+@requires("forum.posts.create")
+def upload_image(request: Request, input: UploadFile = File(...)) -> dict:
+    """Upload an image to imgbb.com"""
+    response: Response = request.state.requests.post(
+        "https://imgbb.com/json",
+        files={
+            "source": (input.filename, input.file.read(), input.content_type or "image"),
+            "type": (None, "file"),
+            "action": (None, "upload"),
+            "timestamp": (None, round(time())),
+            "auth_token": (None, "778d04bbb1eea95cead873eb0adb8921b24dfbc5"),
+        },
+        headers={
+            "referer": "https://imgbb.com/",
+            "accept": "application/json"
+        }
+    )
+
+    return JSONResponse(
+        status_code=response.status_code,
+        content=response.json()
+    )

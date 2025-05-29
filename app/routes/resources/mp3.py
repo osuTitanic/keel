dad71d489735818a185677d21a31896864250415
Lekuru <contact@lekuru.xyz>
2025-05-29 14:35:10 +0200

    Add endpoint for uploading audio preview files

--- app/routes/resources/mp3.py
+++ app/routes/resources/mp3.py
@@ -1,6 +1,9 @@
 
-from fastapi import HTTPException, APIRouter, Request
-from fastapi.responses import StreamingResponse
+from fastapi import HTTPException, APIRouter, Request, UploadFile, File
+from fastapi.responses import StreamingResponse, Response
+from app.common.database import beatmapsets
+from app.security import require_login
+from app.utils import requires
 from io import BytesIO
 
 router = APIRouter()
@@ -16,4 +19,27 @@ def get_internal_mp3(request: Request, filename: str):
     return StreamingResponse(
         BytesIO(file),
         media_type="audio/mpeg"
+    )
+
+@router.put("/mp3/{set_id}", dependencies=[require_login])
+@requires("beatmaps.resources.mt.upload")
+def upload_internal_background(
+    request: Request,
+    set_id: int,
+    mp3: UploadFile = File(...)
+) -> Response:
+    if not (beatmapset := beatmapsets.fetch_one(set_id, request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested beatmap set could not be found"
+        )
+
+    request.state.storage.upload_mp3(
+        beatmapset.id,
+        mp3.file.read(),
+    )
+
+    return Response(
+        status_code=204,
+        headers={"Location": f'/resources/mp3/{beatmapset.id}'}
     )

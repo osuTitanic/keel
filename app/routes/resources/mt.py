92daa562059aad1fff6964958a2b889865bf196c
Lekuru <contact@lekuru.xyz>
2025-05-29 14:35:05 +0200

    Add endpoint for uploading background files

--- app/routes/resources/mt.py
+++ app/routes/resources/mt.py
@@ -1,9 +1,11 @@
 
-from fastapi import HTTPException, APIRouter, Request
-from fastapi.responses import StreamingResponse
+from fastapi import HTTPException, APIRouter, Request, UploadFile, File
+from fastapi.responses import StreamingResponse, Response
+from app.utils import requires, resize_image
+from app.common.database import beatmapsets
+from app.security import require_login
 from datetime import timedelta
 from io import BytesIO
-from app import utils
 
 router = APIRouter()
 
@@ -35,7 +37,7 @@ def get_internal_background_small(request: Request, filename: str):
         )
 
     # Resize image into 80x60
-    file_iterator = utils.resize_image(file, 80, 60)
+    file_iterator = resize_image(file, 80, 60)
 
     # Save in cache
     request.state.storage.save_to_cache(
@@ -47,4 +49,27 @@ def get_internal_background_small(request: Request, filename: str):
     return StreamingResponse(
         file_iterator,
         media_type="image/jpeg"
+    )
+
+@router.put("/mt/{set_id}", dependencies=[require_login])
+@requires("beatmaps.resources.mt.upload")
+def upload_internal_background(
+    request: Request,
+    set_id: int,
+    mt: UploadFile = File(...)
+) -> Response:
+    if not (beatmapset := beatmapsets.fetch_one(set_id, request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested beatmap set could not be found"
+        )
+
+    request.state.storage.upload_background(
+        beatmapset.id,
+        mt.file.read(),
+    )
+
+    return Response(
+        status_code=204,
+        headers={"Location": f'/resources/mt/{beatmapset.id}'}
     )

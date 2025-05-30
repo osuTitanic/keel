29e1a06c86418eac1a443b291a327bd3406ef66e
Lekuru <contact@lekuru.xyz>
2025-05-30 19:43:15 +0200

    Add user reports endpoint

--- app/routes/moderation/users/__init__.py
+++ app/routes/moderation/users/__init__.py
@@ -1,7 +1,10 @@
 
 from app.security import require_login
 from fastapi import APIRouter
+
 from . import infringements
+from . import reports
 
 router = APIRouter(dependencies=[require_login])
 router.include_router(infringements.router)
+router.include_router(reports.router)
---
+++ app/routes/moderation/users/reports.py
@@ -0,0 +1,40 @@
+
+from fastapi import HTTPException, APIRouter, Request
+from app.common.database import reports
+from app.models import ReportModel
+from app.utils import requires
+from typing import List
+
+router = APIRouter()
+
+@router.get("/reports", response_class=List[ReportModel])
+@requires("users.moderation.reports")
+def get_user_reports(request: Request, user_id: int) -> List[ReportModel]:
+    return [
+        ReportModel.model_validate(report, from_attributes=True)
+        for report in reports.fetch_all(user_id, request.state.db)
+    ]
+
+@router.delete("/reports/{id}")
+@requires("users.moderation.reports.delete")
+def resolve_user_report(
+    request: Request,
+    user_id: int,
+    id: int
+) -> ReportModel:
+    if not (report := reports.fetch_by_id(id, request.state.db)):
+        raise HTTPException(404, "Report not found")
+
+    if report.target_id != user_id:
+        raise HTTPException(400, "Report does not belong to the specified user")
+
+    success = reports.update(
+        report.id,
+        {"resolved": True},
+        request.state.db
+    )
+
+    if not success:
+        raise HTTPException(500, "Failed to resolve report")
+
+    return ReportModel.model_validate(report, from_attributes=True)

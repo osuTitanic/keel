c6fa79a8452ad9c45dfb5c74b7c1e4c4a2f8f728
Lekuru <contact@lekuru.xyz>
2025-07-24 21:52:51 +0200

    Add collaboration delete request endpoint

--- app/routes/beatmaps/collaboration.py
+++ app/routes/beatmaps/collaboration.py
@@ -54,7 +54,7 @@ def get_collaboration_requests(request: Request, id: int) -> List[CollaborationR
         for collaboration in collaborations.fetch_requests_outgoing(id, request.state.db)
     ]
 
-@router.post("/{id}/collaborations/requests", response_model=CollaborationRequestModelWithoutBeatmap, dependencies=[require_login])
+@router.post("/{beatmap_id}/collaborations/requests", response_model=CollaborationRequestModelWithoutBeatmap, dependencies=[require_login])
 @requires("users.authenticated")
 def create_collaboration_request(
     request: Request,
@@ -114,3 +114,38 @@ def create_collaboration_request(
         collaboration,
         from_attributes=True
     )
+
+@router.delete("/{beatmap_id}/collaborations/requests/{id}", dependencies=[require_login])
+@requires("users.authenticated")
+def delete_collaboration_request(
+    request: Request,
+    beatmap_id: int,
+    id: int
+) -> dict:
+    if not (beatmap := beatmaps.fetch_by_id(beatmap_id, request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested beatmap could not be found"
+        )
+
+    if beatmap.status <= -3:
+        raise HTTPException(
+            status_code=404,
+            detail="The requested beatmap could not be found"
+        )
+
+    if beatmap.beatmapset.creator_id != request.user.id:
+        raise HTTPException(
+            status_code=403,
+            detail="You are not authorized to perform this action"
+        )
+
+    success = collaborations.delete_request(id, request.state.db)
+
+    if not success:
+        raise HTTPException(
+            status_code=404,
+            detail="The requested invitation could not be found"
+        )
+
+    return {}

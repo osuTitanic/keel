a915571d8dac32228d008100a47532af065741cd
Lekuru <contact@lekuru.xyz>
2025-07-31 13:45:35 +0200

    Optimize server stats route with redis pipelines

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,5 @@
 
-from app.models import ServerStatsModel, BeatmapModeStatsModel
-from fastapi import APIRouter, Request
-
+from fastapi import APIRouter
 from . import beatmapsets
 from . import multiplayer
 from . import moderation
@@ -15,14 +13,13 @@ from . import groups
 from . import scores
 from . import events
 from . import forum
+from . import stats
 from . import users
 from . import oauth
 from . import chat
 
-import app.session
-import time
-
 router = APIRouter(responses={500: {"description": "Internal Server Error"}})
+router.include_router(stats.router)
 router.include_router(chat.router, prefix="/chat", tags=["chat"])
 router.include_router(forum.router, prefix="/forum", tags=["forum"])
 router.include_router(oauth.router, prefix="/oauth", tags=["oauth"])
@@ -39,27 +36,3 @@ router.include_router(moderation.router, prefix="/moderation", tags=["moderation
 router.include_router(beatmapsets.router, prefix="/beatmapsets", tags=["beatmapsets"])
 router.include_router(multiplayer.router, prefix="/multiplayer", tags=["multiplayer"])
 router.include_router(resources.router, prefix="/resources", tags=["internal resources"])
-
-@router.get("/stats", response_model=ServerStatsModel)
-def server_stats(request: Request):
-    return ServerStatsModel(
-        uptime=round(time.time() - app.session.startup_time),
-        online_users=int(request.state.redis.get("bancho:users") or 0),
-        total_users=int(request.state.redis.get("bancho:totalusers") or 0),
-        total_scores=int(request.state.redis.get("bancho:totalscores") or 0),
-        total_beatmaps=int(request.state.redis.get("bancho:totalbeatmaps") or 0),
-        total_beatmapsets=int(request.state.redis.get("bancho:totalbeatmapsets") or 0),
-        beatmap_modes={
-            mode: BeatmapModeStatsModel(
-                mode=mode,
-                count_graveyard=int(request.state.redis.get(f"bancho:totalbeatmaps:{mode}:-2") or 0),
-                count_wip=int(request.state.redis.get(f"bancho:totalbeatmaps:{mode}:-1") or 0),
-                count_pending=int(request.state.redis.get(f"bancho:totalbeatmaps:{mode}:0") or 0),
-                count_ranked=int(request.state.redis.get(f"bancho:totalbeatmaps:{mode}:1") or 0),
-                count_approved=int(request.state.redis.get(f"bancho:totalbeatmaps:{mode}:2") or 0),
-                count_qualified=int(request.state.redis.get(f"bancho:totalbeatmaps:{mode}:3") or 0),
-                count_loved=int(request.state.redis.get(f"bancho:totalbeatmaps:{mode}:4") or 0),
-            )
-            for mode in range(4)
-        }
-    )
---
+++ app/routes/stats.py
@@ -0,0 +1,75 @@
+
+from app.models import ServerStatsModel, BeatmapModeStatsModel
+from fastapi import HTTPException, APIRouter, Request
+from typing import Tuple, Any
+
+import app.session
+import time
+
+router = APIRouter()
+
+@router.get("/stats", response_model=ServerStatsModel)
+def server_stats(request: Request):
+    response = fetch_server_stats(request)
+
+    if not response:
+        raise HTTPException(status_code=500, detail="Failed to fetch server stats")
+
+    return parse_stats_response(response)
+
+def fetch_server_stats(request: Request):
+    keys = [
+        "bancho:users",
+        "bancho:totalusers",
+        "bancho:totalscores",
+        "bancho:totalbeatmaps",
+        "bancho:totalbeatmapsets",
+    ]
+
+    for mode in range(4):
+        keys.extend([
+            f"bancho:totalbeatmaps:{mode}:-2",
+            f"bancho:totalbeatmaps:{mode}:-1",
+            f"bancho:totalbeatmaps:{mode}:0",
+            f"bancho:totalbeatmaps:{mode}:1",
+            f"bancho:totalbeatmaps:{mode}:2",
+            f"bancho:totalbeatmaps:{mode}:3",
+            f"bancho:totalbeatmaps:{mode}:4",
+        ])
+
+    # Use pipeline to fetch all values at once
+    with request.state.redis.pipeline() as pipe:
+        for key in keys:
+            pipe.get(key)
+
+        results = pipe.execute()
+        return results
+
+def parse_stats_response(response: Tuple[Any]) -> ServerStatsModel:
+    response = [int(value) if value is not None else 0 for value in response]
+    offset = 5
+
+    stats = ServerStatsModel(
+        uptime=round(time.time() - app.session.startup_time),
+        online_users=response[0],
+        total_users=response[1],
+        total_scores=response[2],
+        total_beatmaps=response[3],
+        total_beatmapsets=response[4],
+        beatmap_modes={}
+    )
+
+    for mode in range(4):
+        stats.beatmap_modes[mode] = BeatmapModeStatsModel(
+            mode=mode,
+            count_graveyard=response[offset],
+            count_wip=response[offset + 1],
+            count_pending=response[offset + 2],
+            count_ranked=response[offset + 3],
+            count_approved=response[offset + 4],
+            count_qualified=response[offset + 5],
+            count_loved=response[offset + 6]
+        )
+        offset += 7
+        
+    return stats

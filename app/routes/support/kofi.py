cd3b873d2e4389067a2bc7d01c5fd3ccffb452cf
Lekuru <contact@lekuru.xyz>
2025-08-10 18:50:26 +0200

    Implement automatic donator group assignment when donating via. ko-fi

--- app/routes/support/kofi.py
+++ app/routes/support/kofi.py
@@ -1,12 +1,16 @@
 
 from fastapi import HTTPException, APIRouter, Request, Form
 from fastapi.responses import JSONResponse
+
+from app.common.database.repositories import groups, users
 from app.models.kofi import KofiWebhookData
+from app.common import webhooks, officer
 
 import config
 import json
 
 router = APIRouter()
+donator_group_id = 6
 
 @router.post("/callback")
 def kofi_donation_webhook(
@@ -21,5 +25,41 @@ def kofi_donation_webhook(
     if data.verification_token != config.KOFI_VERIFICATION_TOKEN:
         raise HTTPException(403, detail="Invalid verification token")
 
-    request.state.logger.info(f"Kofi donation received: {data}")
+    request.state.logger.info(f"Ko-Fi donation received from '{data.from_name}' ({data.url})")
+    assign_donator_group(data, request)
+
+    if not data.is_public:
+        return JSONResponse({'success': True}, status_code=200)
+
+    embed = webhooks.Embed(
+        title=f"{data.from_name} supported Titanic!",
+        description=data.message,
+        color=0x00FF37,
+        url=data.url
+    )
+
+    # Announce in events channel
+    officer.event(embed=embed)
+
     return JSONResponse({'success': True}, status_code=200)
+
+def assign_donator_group(data: KofiWebhookData, request: Request) -> bool:
+    user = (
+        users.fetch_by_name_extended(data.from_name, request.state.db) or
+        users.fetch_by_email(data.from_email, request.state.db)
+    )
+
+    if not user:
+        return False
+
+    if user.activated:
+        return False
+
+    if groups.entry_exists(user.id, donator_group_id, request.state.db):
+        return True
+
+    groups.create_entry(
+        user.id, donator_group_id,
+        session=request.state.db
+    )
+    return True

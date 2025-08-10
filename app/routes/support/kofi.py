a04b98c9886f61e2a6f30b7a75ccd8e368d7945a
Lekuru <contact@lekuru.xyz>
2025-08-10 16:47:27 +0200

    Fix request decoding

--- app/routes/support/kofi.py
+++ app/routes/support/kofi.py
@@ -2,16 +2,23 @@
 from fastapi import HTTPException, APIRouter, Request, Form
 from fastapi.responses import JSONResponse
 from app.models.kofi import KofiWebhookData
-from config import KOFI_VERIFICATION_TOKEN
+
+import config
+import json
 
 router = APIRouter()
 
 @router.post("/callback")
 def kofi_donation_webhook(
     request: Request,
-    data: KofiWebhookData = Form(...)
+    data_encoded: str = Form(..., alias="data")
 ) -> JSONResponse:
-    if data.verification_token != KOFI_VERIFICATION_TOKEN:
+    data = KofiWebhookData.model_validate_json(data_encoded)
+
+    if request.headers.get('User-Agent') != 'Kofi.Webhooks':
+        raise HTTPException(403, detail="Unauthorized")
+
+    if data.verification_token != config.KOFI_VERIFICATION_TOKEN:
         raise HTTPException(403, detail="Invalid verification token")
 
     request.state.logger.info(f"Kofi donation received: {data}")

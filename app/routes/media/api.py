1886d484c0298100736853d998e958f702002c05
Lekuru <contact@lekuru.xyz>
2025-11-12 20:44:27 +0100

    Share cloudflare state across api workers

--- app/routes/media/api.py
+++ app/routes/media/api.py
@@ -1,11 +1,14 @@
 
 from app.models import BitviewVideoListing
 from fastapi import HTTPException
+from contextlib import suppress
 
 import requests
 import config
+import redis
 import json
 import time
+import app
 
 class BitviewAPI:
     def __init__(
@@ -14,10 +17,11 @@ class BitviewAPI:
         username: str,
         cloudflare_solver: str | None = None
     ) -> None:
+        self.redis = app.session.redis
         self.cloudflare_solver = cloudflare_solver
         self.endpoint = endpoint
         self.username = username
-        
+
         self.last_response: BitviewVideoListing | None = None
         self.last_response_time: float | None = None
         self.last_response_ttl = 60
@@ -25,6 +29,11 @@ class BitviewAPI:
         self.session.headers.update({
             "User-Agent": f"osuTitanic/keel ({config.DOMAIN_NAME})"
         })
+
+        # Load cloudflare session from redis if available
+        # This will ensure that multiple workers can use the same session
+        self.session_key = f"cloudflare:bitview"
+        self.load_cloudflare_session()
 
     @property
     def cloudflare_session_missing(self) -> bool:
@@ -44,6 +53,9 @@ class BitviewAPI:
             # Use cached response
             return self.last_response
 
+        # Try to get a cached cloudflare session
+        self.load_cloudflare_session()
+
         if self.cloudflare_session_missing:
             # Initialize cloudflare session
             return self.update_cloudflare_session()
@@ -55,6 +67,7 @@ class BitviewAPI:
 
         if response.status_code == 403 and self.cloudflare_solver:
             # Our cloudflare session expired, try to update it
+            self.clear_cloudflare_session()
             return self.update_cloudflare_session()
 
         if response.status_code != 200:
@@ -104,6 +117,9 @@ class BitviewAPI:
                 secure=cookie.get('secure', False)
             )
 
+        # Save to redis for sharing across workers
+        self.cache_cloudflare_session(response_useragent, response_cookies)
+
         # For some reason, this can contain HTML, so we'll try to extract the JSON part
         response_json = data["solution"]["response"]
         response_json = response_json[response_json.index('{'):response_json.rindex('}')+1]
@@ -111,3 +127,42 @@ class BitviewAPI:
         self.last_response = json.loads(response_json)
         self.last_response_time = time.time()
         return self.last_response
+
+    def load_cloudflare_session(self) -> None:        
+        if not (session_data := self.redis.get(self.session_key)):
+            return
+
+        data = json.loads(session_data)
+        user_agent = data.get('user_agent')
+        cookies = data.get('cookies', [])
+
+        if user_agent:
+            self.session.headers.update({
+                "User-Agent": user_agent
+            })
+
+        for cookie in cookies:
+            self.session.cookies.set(
+                cookie['name'],
+                cookie['value'],
+                domain=cookie.get('domain'),
+                path=cookie.get('path'),
+                expires=cookie.get('expires'),
+                secure=cookie.get('secure', False)
+            )
+
+    def cache_cloudflare_session(self, user_agent: str, cookies: list) -> None:
+        session_data = {
+            'cookies': cookies,
+            'user_agent': user_agent,
+            'updated_at': time.time()
+        }
+
+        # Store with a TTL of 30 minutes
+        self.redis.setex(
+            self.session_key, 60*30,
+            json.dumps(session_data)
+        )
+
+    def clear_cloudflare_session(self) -> None:
+        self.redis.delete(self.session_key)

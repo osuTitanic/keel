ca91c1187d455da6ffc761b702ef780f462fd1cc
Lekuru <contact@lekuru.xyz>
2025-12-08 19:48:21 +0100

    Refactor score model structure

--- app/models/score.py
+++ app/models/score.py
@@ -6,7 +6,7 @@ from typing import List
 from .user import UserModelCompact
 from .beatmap import BeatmapModel
 
-class ScoreModel(BaseModel):
+class ScoreModelBase(BaseModel):
     id: int
     user_id: int
     submitted_at: datetime
@@ -33,66 +33,15 @@ class ScoreModel(BaseModel):
     grade: str
     replay_views: int
     failtime: int | None
+
+class ScoreModel(ScoreModelBase):
     beatmap: BeatmapModel
     user: UserModelCompact
 
-class ScoreModelWithoutBeatmap(BaseModel):
-    id: int
-    user_id: int
-    beatmap_id: int
-    submitted_at: datetime
-    mode: int
-    status_pp: int
-    status_score: int
-    client_version: int
-    client_string: str
-    pp: float
-    ppv1: float
-    acc: float
-    total_score: int
-    max_combo: int
-    mods: int
-    perfect: bool
-    passed: bool
-    pinned: bool
-    n300: int
-    n100: int
-    n50: int
-    nMiss: int
-    nGeki: int
-    nKatu: int
-    grade: str
-    replay_views: int
-    failtime: int | None
+class ScoreModelWithoutBeatmap(ScoreModelBase):
     user: UserModelCompact
 
-class ScoreModelWithoutUser(BaseModel):
-    id: int
-    user_id: int
-    submitted_at: datetime
-    mode: int
-    status_pp: int
-    status_score: int
-    client_version: int
-    client_string: str
-    pp: float
-    ppv1: float
-    acc: float
-    total_score: int
-    max_combo: int
-    mods: int
-    perfect: bool
-    passed: bool
-    pinned: bool
-    n300: int
-    n100: int
-    n50: int
-    nMiss: int
-    nGeki: int
-    nKatu: int
-    grade: str
-    replay_views: int
-    failtime: int | None
+class ScoreModelWithoutUser(ScoreModelBase):
     beatmap: BeatmapModel
 
 class ScoreRecordsModel(BaseModel):

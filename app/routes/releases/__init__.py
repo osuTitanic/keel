f5c6ada2a95ed682977137dab7354e4a366c4f54
Lekuru <contact@lekuru.xyz>
2025-12-31 12:39:58 +0100

    Add osu changelog endpoint

--- app/routes/releases/__init__.py
+++ app/routes/releases/__init__.py
@@ -1,9 +1,11 @@
 
 from fastapi import APIRouter
 
+from . import changelog
 from . import releases
 from . import upload
 
 router = APIRouter()
+router.include_router(changelog.router)
 router.include_router(releases.router)
 router.include_router(upload.router)
---
+++ app/routes/releases/changelog.py
@@ -0,0 +1,34 @@
+
+from fastapi import APIRouter, Request, Query
+from datetime import datetime
+from typing import List
+
+from app.common.database import changelog
+from app.models import OsuChangelogModel
+
+router = APIRouter()
+
+# TODO: Parse this from configuration
+client_cutoff = datetime(2015, 12, 30)
+
+@router.get("/changelog", response_model=List[OsuChangelogModel])
+def get_osu_changelog(
+    request: Request,
+    start: datetime = Query(client_cutoff),
+    limit: int = Query(100, ge=1, le=500),
+    offset: int = Query(0, ge=0)
+) -> List[OsuChangelogModel]:
+    if start > client_cutoff:
+        start = client_cutoff
+
+    return [
+        OsuChangelogModel.model_validate(
+            entry,
+            from_attributes=True
+        )
+        for entry in changelog.fetch_range_desc(
+            start_date=start,
+            limit=limit, offset=offset,
+            session=request.state.db
+        )
+    ]

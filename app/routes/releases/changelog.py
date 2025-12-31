70c8d97661790c57c2a3e0e23dbc17111e419c1c
Lekuru <contact@lekuru.xyz>
2025-12-31 12:47:03 +0100

    Add minimum date check

--- app/routes/releases/changelog.py
+++ app/routes/releases/changelog.py
@@ -10,11 +10,12 @@ router = APIRouter()
 
 # TODO: Parse this from configuration
 client_cutoff = datetime(2015, 12, 30)
+min_date = datetime(2007, 1, 1)
 
 @router.get("/changelog", response_model=List[OsuChangelogModel])
 def get_osu_changelog(
     request: Request,
-    start: datetime = Query(client_cutoff),
+    start: datetime = Query(client_cutoff, ge=min_date),
     limit: int = Query(100, ge=1, le=500),
     offset: int = Query(0, ge=0)
 ) -> List[OsuChangelogModel]:

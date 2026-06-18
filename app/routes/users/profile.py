7517a51408d772f183182e319d88e5fd586c393b
Lekuru <contact@lekuru.xyz>
2026-06-18 20:20:49 +0200

    Add user stats endpoints

--- app/routes/users/profile.py
+++ app/routes/users/profile.py
@@ -1,7 +1,7 @@
 
 from fastapi import HTTPException, APIRouter, Request
-from app.common.database.repositories import users
-from app.models import UserModel
+from app.models import UserModel, StatsModel, ModeAlias
+from app.common.database import users, stats
 
 router = APIRouter()
 
@@ -20,3 +20,29 @@ def get_user_profile(request: Request, user_id: int) -> UserModel:
         )
 
     return UserModel.model_validate(user, from_attributes=True)
+
+@router.get("/{user_id}/stats")
+def get_user_stats(request: Request, user_id: int):
+    if not (user_stats := stats.fetch_all(user_id, session=request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested user stats could not be found"
+        )
+
+    user_stats.sort(
+        key=lambda x: x.mode
+    )
+    return [
+        StatsModel.model_validate(entry, from_attributes=True)
+        for entry in user_stats
+    ]
+
+@router.get("/{user_id}/stats/{mode}")
+def get_user_stats_by_mode(request: Request, user_id: int, mode: ModeAlias):
+    if not (user_stats := stats.fetch_by_mode(user_id, mode.integer, session=request.state.db)):
+        raise HTTPException(
+            status_code=404,
+            detail="The requested user stats could not be found"
+        )
+
+    return StatsModel.model_validate(user_stats, from_attributes=True)

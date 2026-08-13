3dba0502a3d21db195072425dbc7f8a832620d64
Riley <145041122+Riley-3@users.noreply.github.com>
2026-08-13 08:22:13 -0500

    Change ``/replays/most-viewed`` to ``/most-viewed`` for Scores

--- app/routes/scores/__init__.py
+++ app/routes/scores/__init__.py
@@ -2,10 +2,8 @@
 from fastapi import APIRouter
 
 from . import records
-from . import replays
 from . import scores
 
 router = APIRouter()
 router.include_router(records.router)
-router.include_router(replays.router)
 router.include_router(scores.router)
